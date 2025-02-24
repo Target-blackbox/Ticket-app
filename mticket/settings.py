e50b8d82178f7487c231646d3899b73523c1aefc
Target <akatsukiu638@gmail.com>
2025-02-24 11:34:12 +0530

    Added Procfile and updated requirements

--- mticket/settings.py
+++ mticket/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Directory for static files
 ]
+STATICSTORAGE='whitenoise.storage.CompressedManifestStaticFileStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
